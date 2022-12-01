da6f50044f3c2ab41e90bfbc9fc114e4a235eca2
Attila Oláh <NoNameProvided@users.noreply.github.com>
2022-12-01 16:42:54 +0000

    build: bump version to 0.7.0

--- src/install.user.js
+++ src/install.user.js
@@ -7,7 +7,7 @@
 // @website       https://github.com/spreadmonitor-playground/github-logged-time
 // @iconURL       https://spreadmonitor.com/assets/logo.svg
 
-// @version       0.6
+// @version       0.7
 // @downloadURL   https://cdn.jsdelivr.net/gh/spreadmonitor-playground/github-logged-time@latest/src/install.user.js
 
 // @noframes
@@ -20,8 +20,8 @@
 // @grant         GM_xmlhttpRequest
 
 // @require       https://canvasjs.com/assets/script/canvasjs.min.js
-// @require       https://cdn.jsdelivr.net/gh/spreadmonitor-playground/github-logged-time@0.6.0/src/add_style.js
-// @require       https://cdn.jsdelivr.net/gh/spreadmonitor-playground/github-logged-time@0.6.0/src/main.js
+// @require       https://cdn.jsdelivr.net/gh/spreadmonitor-playground/github-logged-time@0.7.0/src/add_style.js
+// @require       https://cdn.jsdelivr.net/gh/spreadmonitor-playground/github-logged-time@0.7.0/src/main.js
 
 // ==/UserScript==
 
